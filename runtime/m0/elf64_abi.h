ce0af232b0029bcf2859b0c63c5af6472f153706
bicheondev <44262309+bicheondev@users.noreply.github.com>
2026-08-06 17:53:11 +0900

    runtime: vendor minimal Linux ELF64 ABI definitions

---
+++ runtime/m0/elf64_abi.h
@@ -0,0 +1,69 @@
+#ifndef HRT_ELF64_ABI_H
+#define HRT_ELF64_ABI_H
+
+#include <stdint.h>
+
+#define EI_NIDENT 16
+#define EI_CLASS 4
+#define EI_DATA 5
+#define ELFCLASS64 2
+#define ELFDATA2LSB 1
+#define ELFMAG "\177ELF"
+#define SELFMAG 4
+
+#define ET_EXEC 2
+#define EM_X86_64 62
+#define PT_LOAD 1
+#define PF_X 1
+#define PF_W 2
+#define PF_R 4
+
+#define AT_NULL 0
+#define AT_PHDR 3
+#define AT_PHENT 4
+#define AT_PHNUM 5
+#define AT_PAGESZ 6
+#define AT_ENTRY 9
+#define AT_UID 11
+#define AT_EUID 12
+#define AT_GID 13
+#define AT_EGID 14
+#define AT_SECURE 23
+#define AT_RANDOM 25
+#define AT_EXECFN 31
+
+typedef uint16_t Elf64_Half;
+typedef uint32_t Elf64_Word;
+typedef uint64_t Elf64_Xword;
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+
+typedef struct {
+    unsigned char e_ident[EI_NIDENT];
+    Elf64_Half e_type;
+    Elf64_Half e_machine;
+    Elf64_Word e_version;
+    Elf64_Addr e_entry;
+    Elf64_Off e_phoff;
+    Elf64_Off e_shoff;
+    Elf64_Word e_flags;
+    Elf64_Half e_ehsize;
+    Elf64_Half e_phentsize;
+    Elf64_Half e_phnum;
+    Elf64_Half e_shentsize;
+    Elf64_Half e_shnum;
+    Elf64_Half e_shstrndx;
+} Elf64_Ehdr;
+
+typedef struct {
+    Elf64_Word p_type;
+    Elf64_Word p_flags;
+    Elf64_Off p_offset;
+    Elf64_Addr p_vaddr;
+    Elf64_Addr p_paddr;
+    Elf64_Xword p_filesz;
+    Elf64_Xword p_memsz;
+    Elf64_Xword p_align;
+} Elf64_Phdr;
+
+#endif
