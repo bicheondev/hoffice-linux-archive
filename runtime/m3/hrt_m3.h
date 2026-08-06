ecbce5d34fbf5ac5cc55e40019ca6ea5240118c1
bicheondev <44262309+bicheondev@users.noreply.github.com>
2026-08-06 18:40:07 +0900

    runtime: define M3 glibc bridge interfaces

---
+++ runtime/m3/hrt_m3.h
@@ -0,0 +1,20 @@
+#ifndef HRT_M3_H
+#define HRT_M3_H
+
+#include "../m1/hrt_m1.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+LoadedElf load_unrelocated_elf(const char *path);
+size_t patch_guest_code(void *address, size_t length,
+                        size_t *rewritten_fs_prefixes);
+size_t patch_loaded_elf(LoadedElf *loaded,
+                        size_t *rewritten_fs_prefixes);
+void initialize_syscall_bridge(const char *root,
+                               const char *guest_program);
+void *build_m3_initial_stack(const char *guest_path,
+                             const LoadedElf *program,
+                             const LoadedElf *interpreter);
+
+#endif
